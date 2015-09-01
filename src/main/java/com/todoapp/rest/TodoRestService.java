dc2c966ce72291ebd084d687f6f468d6003f97ae
Pruthvi Raj <pruthvi.raj1012@gmail.com>
2015-09-01 11:23:25 +1000

    updating unused imports

--- src/main/java/com/todoapp/rest/TodoRestService.java
+++ src/main/java/com/todoapp/rest/TodoRestService.java
@@ -7,8 +7,10 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException; import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response; import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.WebApplicationException; 
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response; 
+import javax.ws.rs.core.UriBuilder;
 import com.todoapp.domain.ToDo;
 import com.todoapp.service.TodoService;
 
@@ -19,13 +21,13 @@ private TodoService todoService;
 @POST
 @Consumes("application/json")
 public Response create(ToDo entity) {
-todoService.create(entity); return Response.created(
-UriBuilder.fromResource(TodoRestService.class) .path(String.valueOf(entity.getId())).build()).build();
-}
+    todoService.create(entity); return Response.created(
+    UriBuilder.fromResource(TodoRestService.class) .path(String.valueOf(entity.getId())).build()).build();
+    }
 @GET
 @Path("/{id:[0-9][0-9]*}") @Produces(MediaType.APPLICATION_JSON)
 public ToDo lookupTodoById(@PathParam("id") long id) {
-ToDo todo = todoService.find(id); if (todo == null) {
+    ToDo todo = todoService.find(id); if (todo == null) {
 throw new WebApplicationException(Response.Status.NOT_FOUND); }
-return todo; }
+    return todo; }
 }
